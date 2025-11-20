5fa1904fa246d06c1a2d9fa5f5cdef51191576c5
Johnathan Spangler <43426412+Johnathan-Spangler@users.noreply.github.com>
2025-11-20 11:49:53 -0800

    Start of Race Logic

---
+++ Assets/Scripts/RaceLogic.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceLogic : MonoBehaviour
+{
+    public float lapNumber/*, */;
+    public float lapTime;
+    public bool lapped;
+
+    public GameObject[] lapObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lapNumber = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (lapped)
+        {
+            lapObject = GameObject.FindGameObjectsWithTag("Start");
+            //lapObject[0].SetActive = true;
+        }
+        if (other.tag == "Start")
+        {
+            lapNumber++;
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.tag == "End")
+        {
+            lapped;
+        }
+    }
+}
