8ddf32f855f17ec376050548566fe2cdc0f7a3a9
Johnathan Spangler <43426412+Johnathan-Spangler@users.noreply.github.com>
2025-12-04 01:59:39 -0800

    Vehicle Prefab's Cam is Nearly Fixed (Still stutter-y for some reason)

--- Assets/Scripts/CPUController.cs
+++ Assets/Scripts/CPUController.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Controller { Computer, Player }
+
 public class CPUController : MonoBehaviour
 {
+    public Controller controller;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/CameraLogic.cs
+++ Assets/Scripts/CameraLogic.cs
@@ -2,27 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
+using TMPro;
 
 public class CameraLogic : MonoBehaviour
 {
     public Transform car;
-    public float smoothSpeed = 0.125f; // Adjust for smoothness
-    public Vector3 offset;
+    public CPUController AiControl;
+    public float rotationSmoothTime = 0.2f, rotationSmooth = 0.25f;
+    public Vector3 velocity = Vector3.zero, offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     void LateUpdate()
     {
-        if (car != null)
+        if (car == null || AiControl == null) return;
+
+        if (AiControl.controller == Controller.Player)
         {
-            Vector3 desiredPosition = car.position + offset;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            transform.position = smoothedPosition;
-            //transform.LookAt(car);
+            //Camera follow the car
+            Vector3 desiredPos = car.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, rotationSmoothTime);
+
+            //Camera face the car
+            Quaternion targetRot = Quaternion.LookRotation(car.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSmooth * Time.deltaTime);
+
+        }
+        else if (AiControl.controller == Controller.Computer)
+        {
+            gameObject.SetActive(false);
         }
     }
 }
